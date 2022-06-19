ad7c1e9cff7e4fa47cd20e45221426aaf45e02ee
budou114 <58349030+budou114@users.noreply.github.com>
2022-06-19 16:03:24 +0900

    organismの作成2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecures/SearchInput';
 import { UserCard } from './components/organisms/user/UserCard';
+import './App.css';
+
+const user = {
+  name: "じゃけえ",
+  image: "https://source.unsplash.com/2l0CWTpcChI",
+  email: "12345@emxample.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,7 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
-      <UserCard />
+      <UserCard user={user} />
     </div>
   );
 };
