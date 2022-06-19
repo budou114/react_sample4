5d21830692507450c1d07c63a48f5f1db2cd006e
budou114 <58349030+budou114@users.noreply.github.com>
2022-06-19 17:03:47 +0900

    Pageの作成

--- src/App.js
+++ src/App.js
@@ -3,31 +3,12 @@ import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecures/SearchInput';
 import { UserCard } from './components/organisms/user/UserCard';
 import { HeaderOnly } from './components/templates/HeaderOnly';
-import './App.css';
-import { BrowserRouter } from 'react-router-dom';
 import { DefaultLayout } from './components/templates/DefaultLayout';
-
-const user = {
-  name: "じゃけえ",
-  image: "https://source.unsplash.com/2l0CWTpcChI",
-  email: "12345@emxample.com",
-  phone: "090-1111-2222",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
+import { Router } from './router/Router';
+import './App.css';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
+    <Router />
   );
 };
