34f5c054ec4f56458b428804da6e9d725ba25e79
budou114 <58349030+budou114@users.noreply.github.com>
2022-06-20 09:27:37 +0900

    contextでのstate管理(基本的な使い方)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import { PrimaryButton } from './components/atoms/button/PrimaryButton';
-import { SecondaryButton } from './components/atoms/button/SecondaryButton';
-import { SearchInput } from './components/molecures/SearchInput';
-import { UserCard } from './components/organisms/user/UserCard';
-import { HeaderOnly } from './components/templates/HeaderOnly';
-import { DefaultLayout } from './components/templates/DefaultLayout';
 import { Router } from './router/Router';
 import './App.css';
+import { UserProvider } from './providers/UserProvider';
 
 export default function App() {
   return (
-    <Router />
+    <UserProvider>
+      <Router />
+    </UserProvider>
   );
 };
